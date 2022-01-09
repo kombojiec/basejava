6f1299e942a4d291f6cb1372aa7fdbef8b36bcb3
Kulakov Pavel <kombojiec@gmail.com>
2022-01-09 20:42:17 +0300

    hw8(feat): create Main class and recursion method

---
+++ src/com/resume/app/Main.java
@@ -0,0 +1,24 @@
+package com.resume.app;
+
+import java.io.File;
+import java.io.IOException;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        File root =  new File("src/");
+        printPacage(root);
+    }
+
+    public static void printPacage(File folder) throws IOException {
+        System.out.println("Directory: " + folder.getName());
+        File[] files =  folder.listFiles();
+        for(File file: files) {
+            if(file.isDirectory()) {
+                printPacage(file);
+            } else {
+                System.out.println("\tFile: " + file.getName());
+            }
+        }
+    }
+
+}
