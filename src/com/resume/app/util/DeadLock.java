69013d93967c314d5b8ce4b0133d8362cfe44ac2
Kulakov Pavel <kombojiec@gmail.com>
2022-02-15 12:31:41 +0300

    master(hw_11): refactor deadlock

--- src/com/resume/app/util/DeadLock.java
+++ src/com/resume/app/util/DeadLock.java
@@ -6,28 +6,49 @@ public class DeadLock {
 
     public static void main(String[] args) {
 
-        new Thread(() -> {
-            System.out.println("Try to catch lockOne by thread-0");
-            synchronized (lockOne) {
-                System.out.println("lockOne caught by thread-0");
-                System.out.println("Try to catch lockTwo by thread-0");
-                synchronized ((lockTwo)) {
-                    System.out.println("lockOne and lockTwo caught by thread-0");
-                }
-            }
-        }).start();
+        new Thread(new ThreadOne(), "threadOne").start();
+        new Thread(new ThreadTwo(), "threadTwo").start();
 
-        new Thread(() -> {
-            System.out.println("Try to catch lockTwo by thread-1");
-            synchronized (lockTwo) {
-                System.out.println("lockTwo caught by thread-1");
-                System.out.println("Try to catch lockOne by thread-1");
-                synchronized ((lockOne)) {
-                    System.out.println("lockTwo and lockOne caught by thread-1");
-                }
-            }
-        }).start();
+    }
+}
 
+class ThreadOne implements Runnable {
+    @Override
+    public void run() {
+        Thread thread = Thread.currentThread();
+        System.out.println("Try to catch lockOne by " + thread.getName());
+        synchronized (DeadLock.lockOne) {
+            try {
+                Thread.sleep(500);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            System.out.println("lockOne caught by " + thread.getName());
+            System.out.println("Try to catch lockTwo by " + thread.getName());
+            synchronized ((DeadLock.lockTwo)) {
+                System.out.println("lockOne and lockTwo caught by " + thread.getName());
+            }
+        }
+    }
+}
+
+class ThreadTwo implements Runnable {
+    @Override
+    public void run() {
+        Thread thread = Thread.currentThread();
+        System.out.println("Try to catch lockTwo by " + thread.getName());
+        synchronized (DeadLock.lockTwo) {
+            try {
+                Thread.sleep(500);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            System.out.println("lockTwo caught by " + thread.getName());
+            System.out.println("Try to catch lockOne by " + thread.getName());
+            synchronized ((DeadLock.lockOne)) {
+                System.out.println("lockTwo and lockOne caught by " + thread.getName());
+            }
+        }
     }
 }
 
