8c6e219870b815d8149d7c86976b8ad7265f0556
Kulakov Pavel <kombojiec@gmail.com>
2022-03-28 22:52:29 +0300

    master(refact): HW12 refact methods after review

--- src/com/resume/app/Main.java
+++ src/com/resume/app/Main.java
@@ -2,17 +2,12 @@ package com.resume.app;
 
 import java.io.File;
 import java.io.IOException;
-import java.util.Arrays;
-import java.util.List;
-import java.util.Optional;
-import java.util.stream.Collectors;
 
 
 public class Main {
     public static void main(String[] args) throws IOException {
         File root =  new File("src/");
         printFilesHierarchy(root, 0);
-
 
     }
 
@@ -29,24 +24,4 @@ public class Main {
             }
         }
     }
-
-    public static int minValue(int[] values) {
-        Optional<Integer> result = Arrays.stream(values)
-                .distinct()
-                .boxed()
-                .sorted()
-                .reduce((a, b) -> (a * 10) + b);
-        if(result.isPresent()) {
-            return result.get();
-        } else throw new IllegalArgumentException("Incorrect data");
-    }
-
-    public static List<Integer> oddOrEven(List<Integer> integers) {
-        int result = integers.stream().reduce(Integer::sum).get();
-
-        return integers.stream()
-                .filter(el -> (result % 2 == 0 && el % 2 != 0) || (result % 2 != 0 && el % 2 == 0))
-                .collect(Collectors.toList());
-    }
-
 }
---
+++ src/com/resume/app/MainStream.java
@@ -0,0 +1,29 @@
+package com.resume.app;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class MainStream {
+
+    public static void main(String[] args) {
+        System.out.println(minValue(new int[]{1, 2, 3, 3, 2, 3}));
+        System.out.println(oddOrEven(List.of(1, 2, 3, 4, 5)));
+    }
+
+    public static int minValue(int[] values) {
+        return Arrays.stream(values)
+                .distinct()
+                .sorted()
+                .reduce(0, (a, b) -> (a * 10) + b);
+    }
+
+    public static List<Integer> oddOrEven(List<Integer> integers) {
+        int sum = integers.stream().reduce(Integer::sum).get();
+
+        return integers.stream()
+                .filter(el -> ((sum % 2 == 0) == (el % 2 != 0)))
+                .collect(Collectors.toList());
+    }
+
+}
