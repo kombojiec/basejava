ba1dd12b8d214ed2a189a2d43b99db5c7c5d5ae7
Kulakov Pavel <kombojiec@gmail.com>
2022-03-28 15:41:44 +0300

    master(feat): HW12 create minValue & oddOrEven methods

--- src/com/resume/app/Main.java
+++ src/com/resume/app/Main.java
@@ -2,11 +2,18 @@ package com.resume.app;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
 
 public class Main {
     public static void main(String[] args) throws IOException {
         File root =  new File("src/");
         printFilesHierarchy(root, 0);
+
+
     }
 
     public static void printFilesHierarchy(File src, int depth) {
@@ -21,6 +28,25 @@ public class Main {
                 System.out.print(file.toString().indent(depth + 4));
             }
         }
+    }
+
+    public static int minValue(int[] values) {
+        Optional<Integer> result = Arrays.stream(values)
+                .distinct()
+                .boxed()
+                .sorted()
+                .reduce((a, b) -> (a * 10) + b);
+        if(result.isPresent()) {
+            return result.get();
+        } else throw new IllegalArgumentException("Incorrect data");
+    }
+
+    public static List<Integer> oddOrEven(List<Integer> integers) {
+        int result = integers.stream().reduce(Integer::sum).get();
+
+        return integers.stream()
+                .filter(el -> (result % 2 == 0 && el % 2 != 0) || (result % 2 != 0 && el % 2 == 0))
+                .collect(Collectors.toList());
     }
 
 }
